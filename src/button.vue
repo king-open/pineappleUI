0605a1ca90a871b2f9be6b9ee8ff7658b3f9fb78
Wang-zhetao <wzt15857662906@163.com>
2022-05-28 07:52:38 +0800

    填写 icon & iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,30 +1,35 @@
 <template>
   <!-- 单文件组件 -->
-  <button class="j-button">按钮</button>
+  <button class="j-button" :class="{[`icon-${iconPosition}`]: true}">
+     <svg v-if="icon" class="icon">
+        <use :xlink:href="`#i-${icon}`" />
+      </svg>
+      <div class="content">
+        <slot/>
+      </div>
+  </button>
 </template>
 
 <script>
   export default {
-   
+   props:['icon','iconPosition']
 }
 </script>
 
 <style lang="scss" scoped>
-.j-button{
-      font-size: var(--font-size);
-      height: var(--button-height);
-      padding: 0 1em;
-      border-radius: var(--border-radius);
-      border: 1px solid var(--border-color);
+.j-button{font-size: var(--font-size); height: var(--button-height);padding: 0 1em;
+      border-radius: var(--border-radius);border: 1px solid var(--border-color);
       background: var(--button-bg);
-      &:hover{
-      border-color: var(--border-color-hover);
-    }
-    &:active{
-      background-color: var(--button-active-bg);
-    }
-    &:focus{
-      outline: none;
+      display: inline-flex; justify-content: center; align-items: center;
+      vertical-align: middle; // 因为是inline-flex 引起的
+      &:hover{border-color: var(--border-color-hover);}
+    &:active{background-color: var(--button-active-bg);}
+    &:focus{outline: none;}
+    > .content{order: 2;}
+     > .icon{order: 1; margin-right: .1em;}
+    &.icon-right{
+      > .icon{order: 2;}
+        >.content{order: 1; margin-left: .1em; }
     }
   }
 
