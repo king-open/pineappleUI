7628409c08381c54323ddf226bd2c473ca6f6c38
Wang-zhetao <wzt15857662906@163.com>
2022-05-28 13:14:13 +0800

    新增 Icon.vue

---
+++ src/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+   <svg  class="j-icon">
+        <use :xlink:href="`#i-${name}`" />
+    </svg>
+</template>
+
+<script>
+  export default {
+    props:['name']
+  }
+</script>
+
+<style lang="scss">
+.j-icon{
+  width: 1em;
+  height: 1em;
+}
+</style>
+
