80a720fad18c736d04317162d30eee5e5e87c2fb
Wang-zhetao <wzt15857662906@163.com>
2022-05-28 13:12:43 +0800

    将svg 代码整理到 icon.vue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Button from './button.vue'
+import Icon from './Icon.vue'
 
 Vue.component('j-button', Button)
+Vue.component('j-icon', Icon)
 
 new Vue({
   el: '#app',
--- src/button.vue
+++ src/button.vue
@@ -1,45 +1,71 @@
 <template>
   <!-- 单文件组件 -->
-  <button class="j-button" :class="{[`icon-${iconPosition}`]: true}">
-     <svg v-if="icon" class="icon">
-        <use :xlink:href="`#i-${icon}`" />
-      </svg>
-      <div class="content">
-        <slot/>
-      </div>
+  <button class="j-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <j-icon v-if="icon" :name="icon"></j-icon>
+    <div class="content">
+      <slot />
+    </div>
   </button>
 </template>
 
 <script>
-  export default {
-   props:{
-     icon:{},
-      iconPosition:{
-        type:String,
-        default:'left',
-        validator (value) {
-          return value === 'left' || value === 'right'
-        }
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return value === 'left' || value === 'right'
       }
-   }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.j-button{font-size: var(--font-size); height: var(--button-height);padding: 0 1em;
-      border-radius: var(--border-radius);border: 1px solid var(--border-color);
-      background: var(--button-bg);
-      display: inline-flex; justify-content: center; align-items: center;
-      vertical-align: middle; // 因为是inline-flex 引起的
-      &:hover{border-color: var(--border-color-hover);}
-    &:active{background-color: var(--button-active-bg);}
-    &:focus{outline: none;}
-    > .content{order: 2;}
-     > .icon{order: 1; margin-right: .1em;}
-    &.icon-right{
-      > .icon{order: 2;}
-        >.content{order: 1; margin-left: .1em; }
-    }
+.j-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle; // 因为是inline-flex 引起的
+
+  &:hover {
+    border-color: var(--border-color-hover);
   }
 
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  >.content {
+    order: 2;
+  }
+
+  >.icon {
+    order: 1;
+    margin-right: .1em;
+  }
+
+  &.icon-right {
+    >.icon {
+      order: 2;
+    }
+
+    >.content {
+      order: 1;
+      margin-left: .1em;
+    }
+  }
+}
 </style>
