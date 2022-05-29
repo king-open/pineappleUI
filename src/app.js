d130ebda71061b02eb236172580a5f9306490609
Wang-zhetao <wzt15857662906@163.com>
2022-05-29 19:22:58 +0800

    创建 button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('j-button', Button)
 Vue.component('j-icon', Icon)
+Vue.component('j-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
